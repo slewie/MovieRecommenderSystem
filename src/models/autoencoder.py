b70a42377b4d1434a39314c2d4c0f3ac217f9817
slewie <vladkowww@gmail.com>
2023-12-02 22:57:24 +0300

    create notebook with surprise library and update previous

---
+++ src/models/autoencoder.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+
+
+class DenseAutoencoder(nn.Module):
+    def __init__(self, input_size, emb_size=32):
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(input_size, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, emb_size)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(emb_size, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, input_size),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        embeddings = self.encoder(x)
+        reconstructions = self.decoder(embeddings)
+        return reconstructions
+
+    def encode(self, x):
+        return self.encoder(x)
